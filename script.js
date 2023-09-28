86d70bd099c456b6a508babdfec3fb739b841eff
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-28 12:55:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateSizeInfo() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById("sizeInfo");
+    sizeInfo.innerHTML = `<h1>Width: ${width} Height: ${height}</h1>`;
+}
+window.addEventListener("resize", updateSizeInfo);
+window.onload = updateSizeInfo;
+
+
+
